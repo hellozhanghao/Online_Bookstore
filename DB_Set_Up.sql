f56df7c26dbae1baf336336868e577f01b11e3c2
Zhang Hao <zhangghaoo@gmail.com>
2016-12-02 15:01:14 +0800

    schema start

--- DB_Set_Up.sql
+++ DB_Set_Up.sql
@@ -3,8 +3,16 @@ create database book_store;
 use book_store;
 
 create table User
-(username varchar(14),
+(username varchar(14) primary key,
  password varchar(12));
  
 insert into User values('zhanghao','password');
 insert into User values('foo','bar');
+
+select * from User;
+
+select * from pet;
+
+select * from person;
+
+DROP TABLE `book_store`.`User`;
--- book_store.py
+++ book_store.py
@@ -12,12 +12,33 @@ db = SQLAlchemy(app)
 # DB schemas
 class DB_User(db.Model):
     __tablename__ = 'User'
-    username = db.Column('username', db.CHAR, primary_key=True)
-    password = db.Column('password', db.CHAR)
+    username = db.Column('username', db.CHAR(20), primary_key=True)
+    password = db.Column('password', db.CHAR(20), primary_key=True)
+    credit_card = db.Column('credit_card', db.CHAR(20))
+    address = db.Column('address',db.CHAR(20))
+    phone = db.Column('phone',db.CHAR(20))
 
-    def __init__(self, username, password):
+    def __init__(self, username, password,credit_card,address,phone):
         self.username = username
         self.password = password
+        self.credit_card=credit_card
+        self.address = address
+        self.phone = phone
+
+
+
+
+# db.create_all()
+# db.session.commit()
+#
+# some_user = DB_User('zhanghao','password','34523423432','345345kasfjsd RD','435346545')
+# db.session.add(some_user)
+# db.session.commit()
+
+user1 = DB_User.query.filter_by(username='zhanghao',password='password').first()
+print(user1.username)
+
+
 
 # Example insert
 # user=DB_User('someone','password')
@@ -37,40 +58,41 @@ class DB_User(db.Model):
 
 
 
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key= True)
-    name = db.Column(db.String(20))
-    pets = db.relationship('Pet', backref= 'owner', lazy = 'dynamic')
-
-
-class Pet(db.Model):
-    id = db.Column(db.Integer, primary_key= True)
-    name = db.Column(db.String(20))
-    owner_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+# class Person(db.Model):
+#     id = db.Column(db.Integer, primary_key= True)
+#     name = db.Column(db.String(20))
+#     pets = db.relationship('Pet', backref= 'owner', lazy = 'dynamic')
+#
+#
+# class Pet(db.Model):
+#     id = db.Column(db.Integer, primary_key= True)
+#     name = db.Column(db.String(20))
+#     owner_id = db.Column(db.Integer, db.ForeignKey('person.id'))
 
 
 # db.create_all()
 # db.session.commit()
 
 
-person_one = Person(name='Anthony')
-person_two = Person(name='Cindy')
-db.session.add(person_one)
-db.session.add(person_two)
-db.session.commit()
+# person_one = Person(name='Anthony')
+# person_two = Person(name='Cindy')
+# db.session.add(person_one)
+# db.session.add(person_two)
+# db.session.commit()
+#
+#
+# pet_one = Pet(name ='Spot', owner=person_one)
+# db.session.add(pet_one)
+# db.session.commit()
 
-
-pet_one = Pet(name ='Spot', owner=person_one)
-db.session.add(pet_one)
-db.session.commit()
-
-
-
-
-
-
-
-
+#
+# user = Person.query.filter_by(id=1).first()
+# print(user.name)
+#
+#
+#
+#
+#
 
 
 
